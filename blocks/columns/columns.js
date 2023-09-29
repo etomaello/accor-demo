cb5f8fc4b1a8565bcf8e30b4305d47aa5e74ceb5
Kacem AIT OUAL <kait@adobe.com>
2023-09-29 16:19:39 +0200

    First commit - Columns without caroussel and not responsive yet and buttom not well positionned + h1 h2 h3 and paragraph styling

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -15,4 +15,38 @@ export default function decorate(block) {
       }
     });
   });
+
+  const myColumns = block.querySelectorAll('.columns > div');
+
+  myColumns.forEach((column) => {
+    column.classList.add('column');
+    // Get second div child with text
+    const textDiv = column.children[1];
+    textDiv.classList.add('text-div');
+  });
+
+  const textDivs = block.querySelectorAll('.text-div');
+  textDivs.forEach((textDiv) => {
+    // Get the title of text div
+    const title = textDiv.children[0];
+    title.classList.add('text-div-title');
+
+    // Get the sub title of text div
+    const subTitle = textDiv.children[1];
+    subTitle.classList.add('sub-title');
+
+    // Get the text div list of text div
+    const textDivList = textDiv.children[2];
+    textDivList.classList.add('text-div-list');
+
+    // get the text div button of text div
+    const textDivButton = textDiv.children[3];
+    textDivButton.classList.add('text-div-button');
+  });
+
+  const hr = document.createElement('hr');
+
+  const myColumnsSection = document.querySelector('.section.columns-container');
+
+  myColumnsSection.appendChild(hr);
 }
