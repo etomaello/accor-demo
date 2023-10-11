c8d01092af71677582cef0b70ad8cd0be40d5e5d
Jordan Eisenburger <eisenbur@adobe.com>
2023-10-11 16:07:39 +0200

    [FEATURE] Added styling to banner and restructured the js

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,27 +1,39 @@
 export default function decorate(block) {
   // Update the block's class
   block.className = 'box bloc-enroll-banner';
+
+  // Create div for the title and subtitle
   const textDiv = document.createElement('div');
   textDiv.className = 'bloc-enroll-banner-text';
+
   const title = block.querySelector('h2');
   title.className = 'bloc-enroll-banner-text-title';
+
   const subtitle = block.querySelector('p:not(.button-container)');
   subtitle.className = 'bloc-enroll-banner-text-subtitle';
+
+  // Create div for the link
   const linkDiv = document.createElement('div');
   linkDiv.className = 'bloc-enroll-banner-link';
+
   const link = block.querySelector('a');
   link.className = 'round-button';
   link.setAttribute('data-ga', '1');
   link.setAttribute('target', '_blank');
   link.setAttribute('title', 'New window');
-  // reomve the remaining divs (unwanted)
-  const unwantedDiv = block.querySelector('div > div.button-container')
-    .parentNode.parentNode;
+
+  // Remove the existing divs
+  const unwantedDiv = block.querySelector('div > div');
   if (unwantedDiv) unwantedDiv.remove();
-  // Append elements to the block
+
+  // Append the title and subtitle to textDiv
   textDiv.appendChild(title);
   textDiv.appendChild(subtitle);
+
+  // Append the link to linkDiv
   linkDiv.appendChild(link);
+
+  // Append textDiv and linkDiv to the block
   block.appendChild(textDiv);
   block.appendChild(linkDiv);
 }
